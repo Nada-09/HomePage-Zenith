aceec3d4cf863339121194cf574e6d2bda72ac17
Nada-09 <nadaalmajidi4@gmail.com>
2024-10-05 14:42:48 +0300

    Initial Commit

---
+++ HomePage-Zenith/HomePage_ZenithApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePage_ZenithApp.swift
+//  HomePage-Zenith
+//
+//  Created by Nada Abdullah on 02/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HomePage_ZenithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomePage-ZenithTests/HomePage_ZenithTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomePage_ZenithTests.swift
+//  HomePage-ZenithTests
+//
+//  Created by Nada Abdullah on 02/04/1446 AH.
+//
+
+import Testing
+@testable import HomePage_Zenith
+
+struct HomePage_ZenithTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
